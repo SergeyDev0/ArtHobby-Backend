e6318f6d7af5d8208c572ce4201270452d01a099
Vladyslav Melnikov <vladmelnikov2003@gmail.com>
2022-12-22 19:57:34 +0100

    @Transactional add

--- src/main/java/com/example/arthobby/controller/ProductController.java
+++ src/main/java/com/example/arthobby/controller/ProductController.java
@@ -6,40 +6,37 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import javax.transaction.Transactional;
 import java.io.IOException;
 import java.util.List;
 
 @RestController
 @CrossOrigin
+@Transactional
 public class ProductController {
 
     @Autowired
     ProductService productService;
-
-    @CrossOrigin
     @GetMapping("/getAllProducts")
+
     public List<Product> getAllProducts(Model model) {
         List<Product> products = productService.getAllProduct();
         model.addAttribute("products", products);
         return productService.getAllProduct();
     }
 
-    @CrossOrigin
     @GetMapping("/getProduct/{id}")
     public Product getProductById(@PathVariable Long id, Model model) {
         model.addAttribute("cakes", productService.getProductById(id));
         return productService.getProductById(id);
     }
-
-    @CrossOrigin
     @PostMapping("/saveProduct")
     public void saveProduct(@RequestBody Product product) throws IOException {
         productService.saveProduct(product);
     }
 
-    @CrossOrigin
     @GetMapping("/filterByDate")
-    public List<Product> filterByDate() {
+    public List<Product> filterByDate(){
         return productService.filterByDateDESC();
     }
 }
